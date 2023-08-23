2ebfce32ec7cc714ad9126aab6bbfceb9a6bc416
Sharlye Vieira de Melo Júnior <77028067+Sharlye@users.noreply.github.com>
2023-08-23 12:32:39 -0300

    If else

---
+++ ifelse.js
@@ -0,0 +1,8 @@
+var vel = 58
+console.log(`A velocidade do seu automovel e de ${vel}Km/h`)
+if(vel > 60) {
+    console.log(`Você ultrapassaou a velocidade maxima permitida. Multado!`)
+} else {
+    console.log(`Parabens, voce esta na velocidade permitida`)
+}
+console.log (`Dirija sempre com prudencia!`)
